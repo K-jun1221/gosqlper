9be377c161df145604b4061d89fedc1c1781241e
K-jun <poittyo091221@gmail.com>
2019-09-28 17:33:12 +0900

    feat: query method almost done!

--- lib/select.go
+++ lib/select.go
@@ -39,6 +39,50 @@ func QueryRow(db *sql.DB, sql SelectSQL, obj interface{}) error {
 
 	return nil
 }
+
+func Query(db *sql.DB, sql SelectSQL, obj interface{}, objs interface{}) error {
+
+	mapping, err := tagCheck(sql.Select, obj)
+	if err != nil {
+		return err
+	}
+	rawSQL, err := sql.MakeSQL()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("mapping: ", mapping)
+	fmt.Println("rawSQL: ", rawSQL)
+
+	rows, err := db.Query(rawSQL)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		columns := make([]interface{}, len(sql.Select))
+		for i := 0; i < len(sql.Select); i++ {
+			var str string
+			columns[i] = &str
+		}
+		err = rows.Scan(columns...)
+		if err != nil {
+			return err
+		}
+
+		v := reflect.Indirect(reflect.ValueOf(obj))
+
+		for i, column := range columns {
+			subv := v.Field(mapping[i])
+			str, _ := column.(*string)
+			subv.SetString(*str)
+		}
+
+		// TODO いい感じに配列で返す
+		fmt.Println(v)
+		// objs = append(objs, obj)
+
+		// TODO END いい感じに配列で返す
 	}
 
 	return nil
